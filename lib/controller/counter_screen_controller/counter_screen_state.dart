199a632db296458084c9caa3149e2e2241862da3
Sreejith-k-c <kcsreejith17120@gmail.com>
2024-12-19 17:11:27 +0530

    changes

--- lib/controller/counter_screen_controller/counter_screen_state.dart
+++ lib/controller/counter_screen_controller/counter_screen_state.dart
@@ -1,7 +1,7 @@
 class CounterScreenState {
   int count;
   String name;
-  CounterScreenState({required this.count, this.name = "user"});
+  CounterScreenState({required this.count, this.name = "User"});
 
   CounterScreenState copyWith({int? newCount, String? newName}) {
     return CounterScreenState(count: newCount ?? count, name: newName ?? name);
